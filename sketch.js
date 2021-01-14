2b345222240fa967282bfe479e6febbe77427aeb
AkbarM08 <72665203+AkbarM08@users.noreply.github.com>
2021-01-13 22:15:56 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function draw(){
   textSize(15);
   fill("White"); 
   text("Note : Press up arrow to feed Drago milk", 130, 100);
-text("You have " + foodS + " milk bottles left", 100, 300);
+text("You have " + foodS + " milk bottles left", 150, 350);
 }
 
 function readStock(data){
